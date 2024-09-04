280336cac5f55fe190e6a3c6b8d7a93a46c594b6
FM22 <artem.khovanov@gmail.com>
2024-09-04 14:29:48 +0100

    optimise imports

--- RealClosedField/RealClosedField/FormallyReal.lean
+++ RealClosedField/RealClosedField/FormallyReal.lean
@@ -1,7 +1,5 @@
 import RealClosedField.Mathlib.Algebra.Order.Ring.Cone
 import RealClosedField.Mathlib.Algebra.Ring.Semireal.Defs
-import Mathlib.Data.Fin.VecNotation
-import Mathlib.Data.Finset.Basic
 
 class IsFormallyReal (R : Type*) [AddCommMonoid R] [Mul R] : Prop where
   eq_zero_of_sum_of_squares_eq_zero {ι : Type} {I : Finset ι} {x : ι → R} {i : ι}
